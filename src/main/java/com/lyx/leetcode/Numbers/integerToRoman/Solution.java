8e9548b3d9d1bc7ebba806b0011e261639285f52
lvyunxiao <lvyunxiao@yidian-inc.com>
2019-08-26 21:07:14 +0800

    update: zigzag-conversion get done

---
+++ src/main/java/com/lyx/leetcode/Numbers/integerToRoman/Solution.java
@@ -0,0 +1,69 @@
+package com.lyx.leetcode.Numbers.integerToRoman;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Classname Solution
+ * @Description Solution
+ * @Date 2019/8/26
+ * @auther lvyunxiao
+ * https://leetcode-cn.com/problems/integer-to-roman/
+ */
+public class Solution {
+    public String intToRoman(int num) {
+        // 把阿拉伯数字与罗马数字可能出现的所有情况和对应关系，放在两个数组中
+        // 并且按照阿拉伯数字的大小降序排列，这是贪心选择思想
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int index = 0;
+        while (index < 13) {
+            // 特别注意：这里是等号
+            while (num >= nums[index]) {
+                // 注意：这里是等于号，表示尽量使用大的"面值"
+                stringBuilder.append(romans[index]);
+                num -= nums[index];
+            }
+            index++;
+        }
+        return stringBuilder.toString();
+    }
+
+    public int romanToInt(String s) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
+
+        int ans = 0;
+        for (int i = 0; i < s.length(); ) {
+            if (i + 1 < s.length() && map.containsKey(s.substring(i, i + 2))) {
+                ans += map.get(s.substring(i, i + 2));
+                i += 2;
+            } else {
+                ans += map.get(s.substring(i, i + 1));
+                i++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = solution.intToRoman(4);
+        System.out.println(s);
+    }
+
+}
---
+++ src/main/java/com/lyx/leetcode/Numbers/letterCombinationsOfAPhoneNumber/Solution.java
@@ -0,0 +1,12 @@
+package com.lyx.leetcode.Numbers.letterCombinationsOfAPhoneNumber;
+
+/**
+ * @Classname Solution
+ * @Description Solution
+ * @Date 2019/8/26
+ * @auther lvyunxiao
+ * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ */
+public class Solution {
+
+}
