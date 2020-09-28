32f553186f29879411f8988b41cbea5c53e9314c
lvyunxiao <lvyunxiao@yidian-inc.com>
2020-09-28 10:48:48 +0800

    update:every day training by 2020-09-28 10:40:00

---
+++ src/main/java/com/lyx/everyDayTraining/lowestCommonAncestorOfABinaryTree/Solution.java
@@ -0,0 +1,77 @@
+package com.lyx.everyDayTraining.lowestCommonAncestorOfABinaryTree;
+
+/*
+236. 二叉树的最近公共祖先
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，
+满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+
+示例 1:
+
+输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+输出: 3
+解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+示例 2:
+
+输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+输出: 5
+解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+
+说明:
+
+所有节点的值都是唯一的。
+p、q 为不同节点且均存在于给定的二叉树中。
+ */
+
+/*
+递归解析：
+终止条件：
+当越过叶节点，则直接返回 null ；
+当 root 等于 p, qp,q ，则直接返回 root ；
+递推工作：
+开启递归左子节点，返回值记为 left ；
+开启递归右子节点，返回值记为 right ；
+返回值： 根据 left 和 right ，可展开为四种情况；
+当 left 和 right 同时为空 ：说明 root 的左 / 右子树中都不包含 p,q ，返回 null ；
+当 left 和 right 同时不为空 ：说明 p, qp,q 分列在 root 的 异侧 （分别在 左 / 右子树），因此 root 为最近公共祖先，返回 root ；
+当 left 为空 ，right 不为空 ：p,q 都不在 root 的左子树中，直接返回 right 。具体可分为两种情况：
+p,q 其中一个在 root 的 右子树 中，此时 right 指向 pp（假设为 pp ）；
+p,q 两节点都在 root 的 右子树 中，此时的 right 指向 最近公共祖先节点 ；
+当 left 不为空 ， right 为空 ：与情况 3. 同理；
+
+作者：jyd
+链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/236-er-cha-shu-de-zui-jin-gong-gong-zu-xian-hou-xu/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+
+
+import com.lyx.leetcode.Tree.TreeNode;
+
+/**
+ * @author lvyunxiao
+ * @date 2020/9/27
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // if (left != null && right != null) {
+        //     return root;
+        // }
+        // return left != null ? left : right;
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+    }
+}
---
+++ src/main/java/com/lyx/everyDayTraining/rotateImage/Solution.java
@@ -0,0 +1,111 @@
+package com.lyx.everyDayTraining.rotateImage;
+
+/*
+48. 旋转图像
+给定一个 n × n 的二维矩阵表示一个图像。
+
+将图像顺时针旋转 90 度。
+
+说明：
+
+你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+
+示例 1:
+
+给定 matrix =
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+原地旋转输入矩阵，使其变为:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+示例 2:
+
+给定 matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+],
+
+原地旋转输入矩阵，使其变为:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+
+ */
+
+
+import java.util.Arrays;
+
+/**
+ * @author lvyunxiao
+ * @date 2020/9/27
+ */
+public class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        int n = matrix.length;
+        int l = 0, r = n - 1, t = 0, b = n - 1;
+        while (l < r && t < b) {
+            rorateEdge(matrix, l++, r--, t++, b--);
+        }
+    }
+
+    public void rorateEdge(int[][] matrix, int cStart, int cEnd, int rStart, int rEnd) {
+        for (int i = 0; i < cEnd - cStart; i++) {
+            int tmp = matrix[rStart][cStart + i];
+            matrix[rStart][cStart + i] = matrix[rEnd - i][cStart];
+            matrix[rEnd - i][cStart] = matrix[rEnd][cEnd - i];
+            matrix[rEnd][cEnd - i] = matrix[rStart + i][cEnd];
+            matrix[rStart + i][cEnd] = tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        // int[][] matrix = {
+        //         { 5, 1, 9,11},
+        //         { 2, 4, 8,10},
+        //         {13, 3, 6, 7},
+        //         {15,14,12,16}
+        // };
+        // int[][] matrix = {
+        //         {2,29,20,26,16,28},
+        //         {12,27,9,25,13,21},
+        //         {32,33,32,2,28,14},
+        //         {13,14,32,27,22,26},
+        //         {4,24,1,6,32,34}
+        // };
+        //[[2,29,20,26,16,28],[12,27,9,25,13,21],[32,33,32,2,28,14],[13,14,32,27,22,26],[33,1,20,7,21,7],[4,24,1,6,32,34]]
+
+        printMatrix(matrix);
+        s.rotate(matrix);
+        printMatrix(matrix);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        System.out.println("========start=========");
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+        System.out.println("=========end ========");
+    }
+}
